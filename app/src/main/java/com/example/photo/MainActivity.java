7f9dc223d3c2ab4998857c3127270be7cb4fb03f
Israel GardenSystem <irios@gardensystem.com.mx>
2021-06-29 15:59:27 -0500

    hasta qui la abre la camara, captura la foto y la muestra en una imegeView y la envia al servidor

---
+++ app/src/main/java/com/example/photo/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.photo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCamara, btnSubir;
+    ImageView photo;
+    public static final  int REQUEST_CODE_PHOTO = 1;
+    String rutaImagen;
+
+
+    private String UPLOAD_URL ="http://192.168.15.30/servicioweb/upload.php";
+    private Bitmap bitmap;
+    private String KEY_IMAGEN = "foto";
+    private String KEY_NOMBRE = "nombre";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnCamara = (Button)findViewById(R.id.btnCamera);
+        btnSubir = (Button)findViewById(R.id.btnUpload);
+        photo = (ImageView)findViewById(R.id.imageView);
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
+
+        btnSubir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+    }
+
+    public void takePhoto(){
+        Intent picture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(picture, REQUEST_CODE_PHOTO);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PHOTO && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap img = (Bitmap) extras.get("data");
+            photo.setImageBitmap(img);
+            btnSubir.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public String getStringImagen(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage(){
+
+        //Mostrar el diálogo de progreso
+        final ProgressDialog loading = ProgressDialog.show(this,"Uploanding...","Wait please...",false,false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+                        //Mostrando el mensaje de la respuesta
+                        Toast.makeText(MainActivity.this, s.toString() , Toast.LENGTH_LONG).show();
+                        btnSubir.setVisibility(View.INVISIBLE);
+                        photo.setImageDrawable(null);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+
+                        //Showing toast
+                        Toast.makeText(MainActivity.this, volleyError.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Convertir bits a cadena
+                bitmap = ((BitmapDrawable) photo.getDrawable()).getBitmap();
+                String imagen = getStringImagen(bitmap);
+
+                //Obtener el nombre de la imagen
+                String nombre = "prueba";//editTextName.getText().toString().trim();
+
+                //Creación de parámetros
+                Map<String,String> params = new Hashtable<String, String>();
+
+                //Agregando de parámetros
+                params.put(KEY_IMAGEN, imagen);
+                params.put(KEY_NOMBRE, nombre);
+
+                //Parámetros de retorno
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+
+
+
+}
